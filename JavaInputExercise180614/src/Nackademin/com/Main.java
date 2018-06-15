5f804b7f1c6b88ca3554e085c50a24f012fc96b4
foureverhh <foureverhh@gmail.com>
2018-06-15 12:01:37 +0200

    Exception of input is null or empty

--- JavaInputExercise180614/src/Nackademin/com/Main.java
+++ JavaInputExercise180614/src/Nackademin/com/Main.java
@@ -12,17 +12,33 @@ public class Main {
         System.out.println("What is your name?");
         String s = input.readLine();
         System.out.println("Hi "+s+" !");
+while(true) {
+            System.out.print("Amount?");
+            System.out.flush();
+            s = input.readLine();
+            if(s.isEmpty()) {
+                System.out.println("User has entered ctrl+z");
+                break;
+            }
+            double b = Double.parseDouble(s);
 
-        System.out.print("Amount?");System.out.flush();
-        s = input.readLine();
-        double b = Double.parseDouble(s);
-        System.out.print("Interest?");System.out.flush();
-        s = input.readLine();
-        double i = Double.parseDouble(s);
-        System.out.print("Years?");System.out.flush();
-        s = input.readLine();
-        double y = Double.parseDouble(s);
-        double m = b*i*y;
-        System.out.println("Money you get would be: "+ m);
+            System.out.print("Interest?");
+            System.out.flush();
+            s = input.readLine();
+             if(s == null)
+             break;
+            double i = Double.parseDouble(s);
+
+            System.out.print("Years?");
+            System.out.flush();
+            s = input.readLine();
+            if(s == null)
+            break;
+            double y = Double.parseDouble(s);
+
+            double m = b * i * y;
+            System.out.println("Money you get would be: " + m
+            );
+        }
     }
 }
